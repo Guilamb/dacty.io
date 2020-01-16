4d2a9de6b5012ec804b94f8f6ad8711f5a53d46a
Cosinus59 <sylvain.camus15@gmail.com>
2020-01-16 17:30:58 +0100

    Update Dacty_io.java

--- src/Dacty_io.java
+++ src/Dacty_io.java
@@ -240,7 +240,7 @@ class Dacty_io extends Program {
 				}
 			} while (((tagada != 1 && (tagada != 2)) && re==0) || (((tagada>600) || (tagada<10)) && re == 3) || (((tagada>400) || (tagada<10)) && re == 6));
 			if (tagada == 2){
-				re = 6;
+				re = 3;
 			} else if (tagada>9) {
 				re = 10;
 			}
